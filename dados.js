82368fb3510ae8a899619f6af8744e9911f22eff
Anna-Paula-Pinheiro <annaq.pinheiro@gmail.com>
2024-09-06 20:21:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados =  [ {
+    titulo: "Pânico",
+    descricao: "Todos os clássicos da franquia estão disponíveis no Amazon Prime, com exceção dos novos filmes. Pânico V está disponível apenas para aluguel, já Pânico VI  está disponível na Netflix. A franquia Pânico revolucionou o gênero de terror ao meta-analisar os próprios clichês do cinema de horror. Com um assassino mascarado conhecido como Ghostface, a série acompanha Sidney Prescott e seus amigos enquanto são perseguidos e testados em seus conhecimentos sobre filmes de terror. A cada nova entrega, a trama se reinventa, mas mantém a essência do suspense, da tensão e da ironia, tornando-se um clássico contemporâneo do terror",
+    genero: "Slasher", 
+    link: "https://www.imdb.com/title/tt0117571/"
+  },
+  {
+    titulo: "A Hora do Pesadelo",
+    descricao: "Pouca gente sabe, mas o clássico de horror estrelado por Freddy Krueger possui 8 filmes, sendo 7 deles lançados em sequência, após o sucesso do primeiro filme 'A Hora do Pesadelo' de 1984 e um remake de mesmo nome, de 2010. O primeiro e segundo filme, assim como o remake, estão disponíveis no Max. Curiosamente, apenas o filme de número 6 da franquia está disponível no Amazon Prime e todos os outros 7 filmes disponíveis para aluguel. Freddy Krueger, com sua luva de garras e pesadelos aterrorizantes, marcou a história do cinema de terror. A franquia acompanha os moradores de Elm Street que são perseguidos em seus sonhos por esse icônico vilão. Com uma atmosfera sombria e elementos sobrenaturais, A Hora do Pesadelo é um clássico que continua a assustar até hoje.",
+    genero: "Slasher",
+    link: "https://www.imdb.com/title/tt0087800/"
+     },
+  {
+    titulo: "Sexta-feira 13",
+    descricao: "A Franquia 'Sexta-feira 13' conta 12 filmes e até mesmo com um cross over entre os mundos de Jason Voorhees e Freddy Krueger. Atualmente todos os filmes estão disponíveis apenas para aluguel no Amazon Prime. Jason Voorhees, com sua máscara de hóquei e machado, é um dos assassinos mais famosos do cinema. A franquia acompanha um grupo de jovens que são perseguidos em um acampamento de verão por este implacável assassino. Com cenas de suspense e gore, Sexta-Feira 13 se tornou um ícone do slasher.",
+    genero: "Slasher",
+    link: "https://www.imdb.com/title/tt0080761/"
+  }
+];
